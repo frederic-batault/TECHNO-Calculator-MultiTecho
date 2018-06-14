cc281438c484920d7d128cc7528286dfab2a1005
Batault Frederic <frederic.batault@gfi.fr>
2018-06-14 13:49:20 +0200

    creation d'un webservice avec spring sans dao

---
+++ WebService-Maven-Spring-SansJpa/src/main/java/domaine/Memoire.java
@@ -0,0 +1,49 @@
+package domaine;
+
+
+public class Memoire {
+
+
+	private int id;
+
+
+	private double memoire;
+
+
+	private String memoireTexte;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getMemoire() {
+		return memoire;
+	}
+
+	public void setMemoire(double memoire) {
+		this.memoire = memoire;
+	}
+
+	public String getMemoireTexte() {
+		return memoireTexte;
+	}
+
+	public void setMemoireTexte(String memoireTexte) {
+		this.memoireTexte = memoireTexte;
+	}
+
+	public Memoire(double memoire, String memoireTexte) {
+		super();
+		this.memoire = memoire;
+		this.memoireTexte = memoireTexte;
+	}
+
+	public Memoire() {
+		super();
+	}
+
+}
