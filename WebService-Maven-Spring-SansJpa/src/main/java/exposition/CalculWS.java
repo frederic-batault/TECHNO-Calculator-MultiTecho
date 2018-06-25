f16699905974a6d3205b8fc01bd11e5034e6ea2a
Batault Frederic <frederic.batault@gfi.fr>
2018-06-25 16:44:52 +0200

    ajout dans les classes qui exposent le webservice 'WebService-Maven-Spring-SansJpa' d'une annotation permettant de consommer ce webservice avec le serveur angular (CORS)

--- WebService-Maven-Spring-SansJpa/src/main/java/exposition/CalculWS.java
+++ WebService-Maven-Spring-SansJpa/src/main/java/exposition/CalculWS.java
@@ -1,6 +1,7 @@
 package exposition;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import service.ICalculService;
 
 @RestController
 @RequestMapping("/calcul")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class CalculWS {
 
 	@Autowired
--- WebService-Maven-Spring-SansJpa/src/main/java/exposition/MemoireWS.java
+++ WebService-Maven-Spring-SansJpa/src/main/java/exposition/MemoireWS.java
@@ -1,6 +1,7 @@
 package exposition;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import service.MemoireService;
 
 @RestController
 @RequestMapping("/memoire")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class MemoireWS {
 
 	@Autowired
